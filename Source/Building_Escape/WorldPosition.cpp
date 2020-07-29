debd67ec7bf160441ebcad8ea13d6ef98541ca3d
pedroiriartew <pedroiriartew@yahoo.com.ar>
2020-07-29 14:36:58 -0300

    Added Door meshes and materials

---
+++ Source/Building_Escape/WorldPosition.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+
+	/*FString Log = TEXT("Hello!");
+	FString* PtrLog = &Log;
+
+	UE_LOG(LogTemp, Warning, TEXT("%s"), **PtrLog);//Dos asteriscos, uno es el que dereferencia al puntero y el otro sobrecarga para poder imprimir*/
+
+	UE_LOG(LogTemp, Warning, TEXT("This component belongs to %s"), *GetOwner()->GetName());
+
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("And its location is %s"), *ObjectPosition);
+
+
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
